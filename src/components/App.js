8703207d3c80077a31c3b89c927f401e7797be9a
lauragonzalezcalvo <laura24gc@gmail.com>
2022-11-28 08:53:37 +0100

    add basic structure

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+// import monigota from '../images/monigota.png';
+import '../styles/App.scss';
+
+function App() {
+  return (
+
+    // VARIABLES ESTADO
+    // USEEFFECT
+    // FUNCIONES HANDLER
+    // FUNCIONES Y VARIABLES QUE AYUDEN A RENDERIZAR
+
+    // RETURN
+    <div className="App">
+      <table className="table">
+      <thead >
+        <tr>
+          <th>Nombre</th>
+          <th>Tutora</th>
+          <th>Especialidad</th>
+        </tr>
+      </thead>
+      <tbody className="table">
+        <tr>
+          <td>MariCarmen</td>
+          <td>Yanelis</td>
+          <td>Python</td>
+        </tr>
+        
+        <tr>
+          <td>Amparo</td>
+          <td>Dayana</td>
+          <td>IA</td>
+        </tr>
+        
+        <tr>
+          <td>Escandia</td>
+          <td>Iván</td>
+          <td>3D graphics</td>
+        </tr>
+      </tbody>
+      </table>
+
+    </div>
+  );
+}
+
+export default App;
