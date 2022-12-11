31847f3d40f30374c0edd68760f99b76622c5c97
lauragonzalezcalvo <laura24gc@gmail.com>
2022-12-11 20:45:10 +0100

    Add the issues

---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+const fetchdataApi = () => {
+  return fetch(
+    "https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/adalabers-v1/promo-radsajsd.json"
+  )
+    .then((response) => response.json())
+    .then((responseData) => {
+      const dataresults = responseData.results;
+      return dataresults;
+    });
+};
+export default fetchdataApi;
